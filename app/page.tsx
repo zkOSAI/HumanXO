98dbdb887ad133d301f81134c071e18ae4178e9f
Reiser9 <elmanamirhanov@mail.ru>
2025-05-20 18:55:08 +0500

    project

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,339 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
 import Image from "next/image";
+import cn from "classnames";
+
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import {
+    Cross,
+    Dots,
+    Face,
+    Home,
+    Home2,
+    Menu,
+    Moon,
+    Star,
+    Stats,
+    Sun,
+} from "@/shared/icons";
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+export default function HomePage() {
+    const [theme, setTheme] = React.useState<"light" | "dark">("light");
+    const [connectedWallet, setConnectedWallet] = React.useState(false);
+    const [mobileMenu, setMobileMenu] = React.useState(false);
+
+    return (
+        <div className={styles.content}>
+            <div
+                className={cn(styles.sidebar, {
+                    [styles.active]: mobileMenu,
+                })}
+            >
+                <div className={styles.sidebarTop}>
+                    <div className={styles.sidebarLogoInner}>
+                        <Link
+                            href="/"
+                            className={styles.sidebarLogo}
+                            onClick={() => setMobileMenu(false)}
+                        >
+                            <Image src="/img/logo.png" alt="logo" fill />
+                        </Link>
+
+                        <button
+                            className={cn(styles.button, styles.sidebarClose)}
+                            onClick={() => setMobileMenu(false)}
+                        >
+                            <Cross />
+                        </button>
+                    </div>
+
+                    <nav className={styles.sidebarNav}>
+                        <Link
+                            href="/"
+                            className={cn(styles.sidebarNavLink, styles.active)}
+                            onClick={() => setMobileMenu(false)}
+                        >
+                            <Home />
+                            Dashboard
+                        </Link>
+
+                        <Link
+                            href="/"
+                            className={styles.sidebarNavLink}
+                            onClick={() => setMobileMenu(false)}
+                        >
+                            <Star />
+                            Reputation
+                        </Link>
+
+                        <Link
+                            href="/"
+                            className={styles.sidebarNavLink}
+                            onClick={() => setMobileMenu(false)}
+                        >
+                            <Stats />
+                            Statistics
+                        </Link>
+                    </nav>
+                </div>
+
+                <div className={styles.sidebarBottom}>
+                    <div className={styles.sidebarBottomTheme}>
+                        <button
+                            className={cn(
+                                styles.button,
+                                styles.sidebarThemeItem,
+                                { [styles.active]: theme === "dark" }
+                            )}
+                            onClick={() => setTheme("dark")}
+                        >
+                            <Moon />
+                        </button>
+
+                        <button
+                            className={cn(
+                                styles.button,
+                                styles.sidebarThemeItem,
+                                { [styles.active]: theme === "light" }
+                            )}
+                            onClick={() => setTheme("light")}
+                        >
+                            <Sun />
+                        </button>
+                    </div>
+
+                    <button
+                        className={cn(styles.button, styles.sidebarBottomMore)}
+                    >
+                        <Dots />
+                    </button>
+                </div>
+            </div>
+
+            <div className={styles.contentArea}>
+                <div className={styles.contentAreaWrapper}>
+                    <div className={styles.contentAreaTop}>
+                        <button
+                            className={cn(
+                                styles.button,
+                                styles.contentAreaMenu
+                            )}
+                            onClick={() => setMobileMenu(true)}
+                        >
+                            <Menu />
+                        </button>
+
+                        <button
+                            className={cn(styles.button, styles.connectButton)}
+                            onClick={() => setConnectedWallet((prev) => !prev)}
+                        >
+                            {connectedWallet
+                                ? "Disonnect Wallet"
+                                : "Connect Wallet"}
+                        </button>
+                    </div>
+
+                    {connectedWallet ? (
+                        <div className={styles.dashboard}>
+                            <div className={styles.dashboardInfo}>
+                                <div
+                                    className={styles.dashboardInfoCircle}
+                                ></div>
+
+                                <p className={styles.dashboardInfoBread}>
+                                    <Home2 />
+                                    Dashboard
+                                </p>
+
+                                <p className={styles.dashboardInfoBalance}>
+                                    100 Zkos
+                                </p>
+
+                                <button
+                                    className={cn(
+                                        styles.button,
+                                        styles.dashboardInfoClaim
+                                    )}
+                                >
+                                    Claim Rewards
+                                </button>
+
+                                <p className={styles.dashboardInfoEarned}>
+                                    Total Rewards Earned All Time
+                                </p>
+
+                                <p className={styles.dashboardInfoValue}>
+                                    7322 Zkos
+                                </p>
+                            </div>
+
+                            <div className={styles.dashboardWrapper}>
+                                <div className={styles.dashboardInvite}>
+                                    <Image
+                                        src="/img/invite-bg.png"
+                                        alt="bg"
+                                        fill
+                                    />
+
+                                    <div
+                                        className={
+                                            styles.dashboardInviteContent
+                                        }
+                                    >
+                                        <p
+                                            className={
+                                                styles.dashboardInviteSoon
+                                            }
+                                        >
+                                            Coming soon
+                                        </p>
+
+                                        <div
+                                            className={
+                                                styles.dashboardInviteTextBlock
+                                            }
+                                        >
+                                            <p
+                                                className={
+                                                    styles.dashboardInviteTitle
+                                                }
+                                            >
+                                                Invite friends, earn rewards!
+                                            </p>
+
+                                            <p
+                                                className={
+                                                    styles.dashboardInviteText
+                                                }
+                                            >
+                                                Refer your friends and get extra
+                                                bonuses when they join.
+                                            </p>
+
+                                            <p
+                                                className={
+                                                    styles.dashboardInviteText
+                                                }
+                                            >
+                                                The more you share, the more you
+                                                earn!
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className={styles.dashboardSync}>
+                                    <div
+                                        className={styles.dashboardSyncCircle}
+                                    ></div>
+
+                                    <p className={styles.dashboardSyncTitle}>
+                                        Sync Browser Extension
+                                    </p>
+
+                                    <p className={styles.dashboardSyncText}>
+                                        To earn and claim your rewards, import
+                                        and sync your HumanXO private key.
+                                    </p>
+
+                                    <button
+                                        className={cn(
+                                            styles.button,
+                                            styles.dashboardSyncButton
+                                        )}
+                                    >
+                                        Import Private Key
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className={styles.welcome}>
+                            <div className={styles.welcomeBlock}>
+                                <div className={styles.welcomeCircle}></div>
+
+                                <div className={styles.welcomeBlockContent}>
+                                    <div
+                                        className={
+                                            styles.welcomeBlockTitleInner
+                                        }
+                                    >
+                                        <p>Welcome to</p>
+
+                                        <p>HumanXO by zkOS.</p>
+                                    </div>
+
+                                    <p className={styles.welcomeBlockText}>
+                                        Connect and start improving your
+                                        on-chain reputation:
+                                    </p>
+
+                                    <p className={styles.welcomeBlockText}>
+                                        By engaging with HumanXO, users
+                                        strengthen their on-chain identity,
+                                        unlock rewards and contribute to a more
+                                        Sybil-resistant blockchain.
+                                    </p>
+
+                                    <button
+                                        className={cn(
+                                            styles.button,
+                                            styles.welcomeBlockConnect
+                                        )}
+                                        onClick={() => setConnectedWallet(true)}
+                                    >
+                                        Connect Wallet
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div className={styles.welcomeWrapper}>
+                                <div className={styles.welcomeSolana}>
+                                    <div className={styles.welcomeSolanaImg}>
+                                        <Image
+                                            src="/img/solana.png"
+                                            alt="Solana"
+                                            fill
+                                        />
+                                    </div>
+
+                                    <div className={styles.welcomeSolanaText}>
+                                        <p>Built on Solana,</p>
+
+                                        <p>designed to increase</p>
+
+                                        <p>on-chain human activity.</p>
+                                    </div>
+                                </div>
+
+                                <div className={styles.welcomeExtension}>
+                                    <Face
+                                        className={styles.welcomeExtensionIcon}
+                                    />
+
+                                    <p className={styles.welcomeExtensionText}>
+                                        Passively verify you&rsquo;re human,
+                                        build reputation in the HumanXO
+                                        ecosystem, and earn rewards.
+                                    </p>
+
+                                    <button
+                                        className={cn(
+                                            styles.button,
+                                            styles.downloadExtension
+                                        )}
+                                    >
+                                        Download Browser Extension
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
